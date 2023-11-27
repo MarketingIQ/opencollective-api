63ed6056117d9e770b38ccfc80aa22ff761cfc19
Benjamin Piouffle <benjamin@opencollective.com>
2023-11-27 10:01:44 +0100

    fix(Expense): enforce null item URL for Invoices

--- server/graphql/common/expenses.ts
+++ server/graphql/common/expenses.ts
@@ -1047,9 +1047,14 @@ const checkExpenseItems = (expenseType, items: (ExpenseItem | Record<string, unk
 
   // If expense is a receipt (not an invoice) then files must be attached
   if (expenseType === EXPENSE_TYPE.RECEIPT) {
-    const hasMissingFiles = items.some(a => !a.url);
-    if (hasMissingFiles) {
+    if (items.some(a => !a.url)) {
       throw new ValidationFailed('Some items are missing a file');
+    }
+  } else if (expenseType === EXPENSE_TYPE.INVOICE) {
+    if (items.some(a => a.url)) {
+      throw new ValidationFailed(
+        'Invoice items cannot have a file attached. To attach documentation. please use `attachedFiles` on the expense instead.',
+      );
     }
   }
 };
